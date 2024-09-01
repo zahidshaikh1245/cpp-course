c46046a286454b5e65d269a6df5e2ce2544d4873
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-09-01 13:26:25 +0530

    day3

---
+++ day3/array.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int array [7]={10,9,65,25,32,25,45};
+}
---
+++ day3/switch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c ='*'
+    switch(){
+        case '1':
+        cout<<"1 value printed";
+        break;
+
+        case '2':
+        cout<<"2 value printed";
+        break;
+
+        case '*':
+        cout<<"* value printed";
+        break;
+
+        case '4':
+        cout<<"4 value printed";
+        break;
+
+        case '5':
+        cout<<"5 value printed";
+        break;
+
+        default:
+        cout<<"25 value printed";
+    }
+}
+
