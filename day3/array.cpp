db744ccb23e5317d04c6e424821217466e022db5
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-09-01 13:30:06 +0530

    Update array.cpp

--- day3/array.cpp
+++ day3/array.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
-    int array [7]={10,9,65,25,32,25,45};
+    int array[7]={10,9,65,25,32,25,45};
+    
+    for(int i=0; i<7; i++){
+        cout<<array[i]<<endl;
+    }
+    
+    
+    char zahid[4]={'t',')','*','@'};
+    for(int i=0; i<4; i++){
+        cout<<zahid[i]<<endl;
+    }
 }
