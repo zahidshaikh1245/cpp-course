0beea48b3d5c9992d3216c1809a35a5979589dca
Shravani3031 <shravanisawant322@gmail.com>
2024-10-06 12:49:20 -0700

    y

---
+++ day6/problems/arrayAverage.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[10]={2,6,1,5,23,6,6,2,6,0};
+    int key=5;
+    for(int i=0; i<10; i++){
+        if(arr[i]==key){
+            cout<<a;
+        }
+
+    }
+}
---
+++ day6/problems/fibonacci.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 1;
+    int b = 1;
+    int c;
+    for(int i=0; i<10; i++){
+        cout<<a<<endl;
+        c=a+b;
+        a=b;
+        b=c;
+
+    }
+}
